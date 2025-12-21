11235ff686f6bf4c5a6ab6cb4528adab32a69362
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-21 13:26:15 +0530

    Initial backend structure with microservices

---
+++ email-service/src/test/java/com/flightapp/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightapp.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
