f96c367cad762ce8cb06fae0543e459a8b2a499f
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-21 18:40:24 +0530

    Implemented the Login logic

--- auth-service/src/main/java/com/flightapp/auth_service/dto/LoginRequest.java
+++ auth-service/src/main/java/com/flightapp/auth_service/dto/LoginRequest.java
@@ -2,4 +2,24 @@ package com.flightapp.auth_service.dto;
 
 public class LoginRequest {
 
+    private String email;
+    private String password;
+
+    public LoginRequest() {}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
