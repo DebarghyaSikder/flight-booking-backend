554f60f2d02534196e10ca7e13f1a40ce1a76442
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-21 17:47:49 +0530

    Corrected the file

--- auth-service/src/main/java/com/flightapp/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/com/flightapp/auth_service/controller/AuthController.java
@@ -1,5 +1,23 @@
 package com.flightapp.auth_service.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.flightapp.auth_service.dto.RegisterRequest;
+import com.flightapp.auth_service.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
 }
