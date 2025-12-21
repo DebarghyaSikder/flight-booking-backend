8f989439175f29e8fc74cbc6e7609ac7cae1cfe4
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-22 01:38:23 +0530

    Implemented flight controller

--- flight-service/src/main/java/com/flightapp/flight_service/service/impl/FlightServiceImpl.java
+++ flight-service/src/main/java/com/flightapp/flight_service/service/impl/FlightServiceImpl.java
@@ -1,5 +1,35 @@
 package com.flightapp.flight_service.service.impl;
 
-public class FlightServiceImpl {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.flightapp.flight_service.entity.Flight;
+import com.flightapp.flight_service.repository.FlightRepository;
+import com.flightapp.flight_service.service.FlightService;
+
+@Service
+public class FlightServiceImpl implements FlightService {
+
+    private final FlightRepository flightRepository;
+
+    public FlightServiceImpl(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
+    @Override
+    public Flight addFlight(Flight flight) {
+        flight.setAvailableSeats(flight.getTotalSeats());
+        return flightRepository.save(flight);
+    }
+
+    @Override
+    public List<Flight> searchFlights(String source, String destination) {
+        return flightRepository.findBySourceAndDestination(source, destination);
+    }
+
+    @Override
+    public Flight getFlightById(Long id) {
+        return flightRepository.findById(id).orElse(null);
+    }
 }
