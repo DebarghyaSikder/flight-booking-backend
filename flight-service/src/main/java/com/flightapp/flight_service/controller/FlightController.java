9ccb90a6351ac047985dbbb2c6193108be470647
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-22 01:46:47 +0530

    Implemented flight controller

--- flight-service/src/main/java/com/flightapp/flight_service/controller/FlightController.java
+++ flight-service/src/main/java/com/flightapp/flight_service/controller/FlightController.java
@@ -1,5 +1,39 @@
 package com.flightapp.flight_service.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.flightapp.flight_service.entity.Flight;
+import com.flightapp.flight_service.service.FlightService;
+
+@RestController
+@RequestMapping("/flight")
 public class FlightController {
 
+    private final FlightService flightService;
+
+    public FlightController(FlightService flightService) {
+        this.flightService = flightService;
+    }
+
+    // Admin use
+    @PostMapping("/add")
+    public Flight addFlight(@RequestBody Flight flight) {
+        return flightService.addFlight(flight);
+    }
+
+    // User search
+    @GetMapping("/search")
+    public List<Flight> searchFlights(
+            @RequestParam String source,
+            @RequestParam String destination) {
+
+        return flightService.searchFlights(source, destination);
+    }
+
+    @GetMapping("/{id}")
+    public Flight getFlight(@PathVariable Long id) {
+        return flightService.getFlightById(id);
+    }
 }
