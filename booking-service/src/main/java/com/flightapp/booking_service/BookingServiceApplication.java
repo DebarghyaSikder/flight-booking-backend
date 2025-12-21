fe62846e050dc2f5ea447ac5b8ac40dec2677ee5
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-22 02:09:29 +0530

    Created Feign client in Booking service

--- booking-service/src/main/java/com/flightapp/booking_service/BookingServiceApplication.java
+++ booking-service/src/main/java/com/flightapp/booking_service/BookingServiceApplication.java
@@ -2,12 +2,12 @@ package com.flightapp.booking_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class BookingServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BookingServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BookingServiceApplication.class, args);
+    }
 }
---
+++ booking-service/src/main/java/com/flightapp/booking_service/client/FlightClient.java
@@ -0,0 +1,5 @@
+package com.flightapp.booking_service.client;
+
+public class FlightClient {
+
+}
