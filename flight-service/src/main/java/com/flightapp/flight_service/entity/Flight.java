d1329838034bc20b21d6c3644e1041edbd27d178
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-22 01:33:35 +0530

    Created Flight Repository

--- flight-service/src/main/java/com/flightapp/flight_service/entity/Flight.java
+++ flight-service/src/main/java/com/flightapp/flight_service/entity/Flight.java
@@ -1,5 +1,108 @@
 package com.flightapp.flight_service.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "flights")
 public class Flight {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String airline;
+
+    private String flightNumber;
+
+    private String source;
+
+    private String destination;
+
+    private String departureTime;
+
+    private String arrivalTime;
+
+    private double price;
+
+    private int totalSeats;
+
+    private int availableSeats;
+
+    public Flight() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getTotalSeats() {
+        return totalSeats;
+    }
+
+    public void setTotalSeats(int totalSeats) {
+        this.totalSeats = totalSeats;
+    }
+
+    public int getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(int availableSeats) {
+        this.availableSeats = availableSeats;
+    }
 }
---
+++ flight-service/src/main/java/com/flightapp/flight_service/repository/FlightRepository.java
@@ -0,0 +1,5 @@
+package com.flightapp.flight_service.repository;
+
+public class FlightRepository {
+
+}
