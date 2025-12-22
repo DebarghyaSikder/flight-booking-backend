2a73eb9fc8873c4d396b79b29a95f8d35beebafb
Debarghya Sikder <debarghya1412@gmail.com>
2025-12-22 11:49:49 +0530

    Published the event from Booking service

--- booking-service/src/main/java/com/flightapp/booking_service/service/impl/BookingServiceImpl.java
+++ booking-service/src/main/java/com/flightapp/booking_service/service/impl/BookingServiceImpl.java
@@ -1,9 +1,11 @@
 package com.flightapp.booking_service.service.impl;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
 import com.flightapp.booking_service.client.FlightClient;
 import com.flightapp.booking_service.entity.Booking;
+import com.flightapp.booking_service.event.BookingConfirmedEvent;
 import com.flightapp.booking_service.repository.BookingRepository;
 import com.flightapp.booking_service.service.BookingService;
 
@@ -12,11 +14,14 @@ public class BookingServiceImpl implements BookingService {
 
     private final BookingRepository bookingRepository;
     private final FlightClient flightClient;
+    private final RabbitTemplate rabbitTemplate;
 
     public BookingServiceImpl(BookingRepository bookingRepository,
-                              FlightClient flightClient) {
+                              FlightClient flightClient,
+                              RabbitTemplate rabbitTemplate) {
         this.bookingRepository = bookingRepository;
         this.flightClient = flightClient;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @Override
@@ -30,13 +35,29 @@ public class BookingServiceImpl implements BookingService {
                 booking.getSeatsBooked()
         );
 
-
         if (seatsUpdated) {
             savedBooking.setStatus("CONFIRMED");
+            bookingRepository.save(savedBooking);
+
+            BookingConfirmedEvent event =
+                    new BookingConfirmedEvent(
+                            savedBooking.getId(),
+                            savedBooking.getUserEmail(),
+                            savedBooking.getFlightId(),
+                            savedBooking.getSeatsBooked()
+                    );
+
+            rabbitTemplate.convertAndSend(
+                    "booking.exchange",
+                    "booking.confirmed",
+                    event
+            );
+
         } else {
             savedBooking.setStatus("CANCELLED");
+            bookingRepository.save(savedBooking);
         }
 
-        return bookingRepository.save(savedBooking);
+        return savedBooking;
     }
 }
